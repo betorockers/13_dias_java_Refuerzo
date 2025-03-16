58ccbccab485adc343d12acefd13e93bf94655a1
betorockers <omar.toledus@gmail.com>
2025-03-16 15:47:10 -0300

    dia_2_up

--- Dia2_listadoPersonas/ListadoPersonasApp/src/ListadoPersonasApp.java
+++ Dia2_listadoPersonas/ListadoPersonasApp/src/ListadoPersonasApp.java
@@ -1,4 +1,68 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<>();
+        // Empezamos con el menu
+        var salir = false;
+        while(!salir){
+            mostrarMenu();
+            try {
+                // mandaremos a llamar la funciona ejecutarOperacion
+                salir = ejecutarOperacion(sc, personas);
+            }catch(Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
+            System.out.println();
+        }
+    }
+    private static void mostrarMenu(){
+        // Mostramos las opciones
+        System.out.print("""
+                **** Listado Personas App ****
+                1. Agregar
+                2. Listar
+                3. Salir
+                """);
+
+        System.out.print("Seleccion una opcion del Menú: ");
+    }
+
+    private static boolean ejecutarOperacion(Scanner sc, List<Persona> personas){
+        var opcion = Integer.parseInt(sc.nextLine());
+        var salir = false;
+        // Revisaremos la opcion proporcionada
+        switch(opcion){
+            case 1 -> { //Agregar persona a la lista
+                System.out.print("Proporciona el Nombre: ");
+                var nombre = sc.nextLine();
+                System.out.print("Proporciona el telefono: ");
+                var tel = sc.nextLine();
+                System.out.print("Proporciona el email: ");
+                var email = sc.nextLine();
+
+                // creamos el objeto del tipo persona
+                var persona = new Persona(nombre, tel, email);
+                // lo agregamos a la lista personas
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + " elementos");
+            }// fin caso 1
+            case 2 -> {// listar las personas
+                System.out.println("Listado de Personas: ");
+                // agregaremos un amejora utilizando lambda y metodo de referencia
+                //personas.forEach((persona) -> System.out.println(persona)); // lambda
+                personas.forEach(System.out::println); // por referencia
+            }
+            case 3 -> {// Salimos del ciclo
+                System.out.println("Hasta pronto...");
+                salir = true;
+            }
+            default -> System.out.println("Opcion erronea: " + opcion);
+        }// fin switch
+        return  salir;
+    }
 }
--- Dia2_listadoPersonas/ListadoPersonasApp/src/Persona.java
+++ Dia2_listadoPersonas/ListadoPersonasApp/src/Persona.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
-
 public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    // Agregaremos un constructor (vacio)
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    // Agregamos un constructor con argumentos
+    public Persona(String nombre, String tel, String email){
+        this(); // mandamos a llamar al constructor vacio
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    // agregaremos los metodos GET Y SET
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // usaremos sobreescritura de metodos de la clase padre
+
+
+    @Override
+    public String toString() {
+        return "Persona:\n" +
+                "id=" + id + '\n' +
+                "nombre='" + nombre + '\'' + '\n' +
+                "tel='" + tel + '\'' + '\n' +
+                "email='" + email + '\'' +
+                '\n';
+    }
+
+    // agreagremos unas pruebas
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan Perez","55443322", "juan@mail.com");
+        System.out.println(persona1);
+    }
 }
